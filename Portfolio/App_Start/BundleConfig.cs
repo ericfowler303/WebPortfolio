2500c7ecbb1d5df497bd42a96bb9e3fd7813f356
Eric Fowler <ericfowler303@gmail.com>
2014-12-05 15:32:23 -0700

    Start adding in the Work section

---
+++ Portfolio/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Portfolio
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/js/jquery.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/js/bootstrap.js",
+                      "~/Scripts/js/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/pdf").Include(
+                        "~/Scripts/pdfobject.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
