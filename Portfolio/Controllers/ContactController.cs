ac2ef077cf8401c2ae8657edb817f0833eddde18
Eric Fowler <ericfowler303@gmail.com>
2014-12-07 15:40:47 -0700

    Finish the ability to submit contact info
    Needs a better resulting page but the info does submit correctly

--- Portfolio/Controllers/ContactController.cs
+++ Portfolio/Controllers/ContactController.cs
@@ -8,10 +8,21 @@ namespace Portfolio.Controllers
 {
     public class ContactController : Controller
     {
+        Models.sp5EricEntities contactDb = new Models.sp5EricEntities();
+
         // GET: Contact
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult SendContact(FormCollection values)
+        {
+            // Add the contact info to the database
+            contactDb.AddNewContact(values["fullname"],values["emailAddress"],values["body"]);
+            contactDb.SaveChanges();
+
+            return Content("Thanks for your submission.");
         }
     }
 }
